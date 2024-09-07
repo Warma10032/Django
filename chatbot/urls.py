4ba9b297e39ec5d1694adc572371c4839750d262
li yanming <Ming86386165@163.com>
2024-09-07 21:41:18 +0800

    完成前后端连接

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from chatbot.views import view, chat,knowledge
+from chatbot.views import knowledge
 
 urlpatterns=[
     path('',view.login, name='login'),
@@ -8,8 +9,10 @@ urlpatterns=[
     path('user/create/', view.user_create, name='user_create'),
     path('users/update/<int:pk>/', view.user_update, name='user_update'),
     path('users/delete/<int:pk>/', view.user_delete, name='user_delete'),
-    path('chat/', chat.grodio_chat_view, name='chat_view'),
-    path('build_knowledge/', knowledge.build_knowledge_view, name='build_knowledge_view'),
-    path('choice/', view.choice_view, name='choice_view')
+    path('register/',view.register),
+    path('upload/', knowledge.build_knowledge_view, name='upload_file'),
+    path('files/', knowledge.list_uploaded_files, name='list_files'),
+    path('files/<str:filename>/', knowledge.delete_file, name='delete_file'),
+    path('view_file/<str:filename>/', knowledge.view_uploaded_file_view, name='view_uploaded_file'),
 
 ]
